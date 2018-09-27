f0f17f89733082efefc49105f4689b5821a6322b
Oleksii <bgeota@gmail.com>
2018-09-27 16:45:24 +0300

    create package projectRepository

--- src/main/java/com/sheet/work/time/table/repository/projectRepository/ProjectRepository.java
+++ src/main/java/com/sheet/work/time/table/repository/projectRepository/ProjectRepository.java
@@ -1,4 +1,4 @@
-package com.sheet.work.time.table.repository;
+package com.sheet.work.time.table.repository.projectRepository;
 
 import com.sheet.work.time.table.vo.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
