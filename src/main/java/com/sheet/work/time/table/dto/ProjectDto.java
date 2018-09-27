50f1c8eedbb13705d108564cffcb1fdb75c4e248
Oleksii <bgeota@gmail.com>
2018-09-27 16:58:52 +0300

    Create ProjectDto

---
+++ src/main/java/com/sheet/work/time/table/dto/ProjectDto.java
@@ -0,0 +1,11 @@
+package com.sheet.work.time.table.dto;
+
+import lombok.Data;
+
+@Data
+public class ProjectDto {
+    private Long id;
+    private String name;
+    private String description;
+    private String address;
+}
