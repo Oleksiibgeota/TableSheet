a95ee9702dfa318b47f2f0b01b3906329bb05fdd
Oleksii <bgeota@gmail.com>
2018-09-27 16:43:31 +0300

    refactorig code

--- src/main/java/com/sheet/work/time/table/repository/employeeRepository/EmployeeRepositoryCustom.java
+++ src/main/java/com/sheet/work/time/table/repository/employeeRepository/EmployeeRepositoryCustom.java
@@ -1,9 +1,5 @@
 package com.sheet.work.time.table.repository.employeeRepository;
 
-import com.sheet.work.time.table.vo.Employee;
-
-import java.util.List;
-
 public interface EmployeeRepositoryCustom {
 //    List<Employee> getFirstNamesLikeAndTeamWorkId(String firstName, Long id);
 }
--- src/main/java/com/sheet/work/time/table/repository/employeeRepository/EmployeeRepositoryImpl.java
+++ src/main/java/com/sheet/work/time/table/repository/employeeRepository/EmployeeRepositoryImpl.java
@@ -1,14 +1,9 @@
 package com.sheet.work.time.table.repository.employeeRepository;
 
-import com.sheet.work.time.table.vo.Employee;
-
-import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import java.util.List;
 
 @Transactional(readOnly = true)
 public class EmployeeRepositoryImpl implements EmployeeRepositoryCustom {
