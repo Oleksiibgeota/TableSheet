a56b86f26bc8406ed8ed640fc1c4530dd02b2fd4
Oleksii <bgeota@gmail.com>
2018-10-03 15:16:58 +0300

    create method getTeamWorkById

--- src/main/java/com/sheet/work/time/table/service/TeamWorkService.java
+++ src/main/java/com/sheet/work/time/table/service/TeamWorkService.java
@@ -1,4 +1,19 @@
 package com.sheet.work.time.table.service;
 
+import com.sheet.work.time.table.repository.teamWorkRepository.TeamWorkRepository;
+import com.sheet.work.time.table.vo.TeamWork;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TeamWorkService {
+    @Autowired
+    TeamWorkRepository teamWorkRepository;
+
+    public TeamWork getTeamWorkById(Long id) throws EntityNotFoundException {
+        if (teamWorkRepository.existsById(id)) {
+            TeamWork teamWork = teamWorkRepository.findById(id).get();
+            return teamWork;
+        } else throw new EntityNotFoundException("teamWork not found by id = " + id);
+    }
 }
