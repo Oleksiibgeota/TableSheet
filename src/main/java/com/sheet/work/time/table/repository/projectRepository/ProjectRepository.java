4c0579b3fe9fae8b76ba1738b500e4fa6a5ebfbf
Oleksii <bgeota@gmail.com>
2018-10-03 10:08:30 +0300

    create query deletProjectByName

--- src/main/java/com/sheet/work/time/table/repository/projectRepository/ProjectRepository.java
+++ src/main/java/com/sheet/work/time/table/repository/projectRepository/ProjectRepository.java
@@ -2,6 +2,9 @@ package com.sheet.work.time.table.repository.projectRepository;
 
 import com.sheet.work.time.table.vo.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    @Transactional
+    void deleteProjectsByName(String name);
 }
