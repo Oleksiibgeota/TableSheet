467288c60feb78b7a2db829caa97af8dfadbf04c
Oleksii <bgeota@gmail.com>
2018-11-21 11:29:05 +0200

    create methods convert Employee

---
+++ src/main/java/com/sheet/work/time/table/ConvertService/ConvertEmployeeService.java
@@ -0,0 +1,24 @@
+package com.sheet.work.time.table.ConvertService;
+
+import com.sheet.work.time.table.dto.EmployeeDto;
+import com.sheet.work.time.table.vo.Employee;
+
+public class ConvertEmployeeService implements ConvertServiceEmployee {
+    @Override
+    public Employee convertEmployee(EmployeeDto employeeDto) {
+        Employee employeeVo = new Employee();
+        employeeVo.setId(employeeDto.getId());
+        employeeVo.setFirstName(employeeDto.getFirstName());
+        employeeVo.setLastName(employeeDto.getLastName());
+        return employeeVo;
+    }
+
+    @Override
+    public EmployeeDto convertEmployee(Employee employee) {
+        EmployeeDto employeeDto = new EmployeeDto();
+        employeeDto.setId(employee.getId());
+        employeeDto.setFirstName(employee.getFirstName());
+        employeeDto.setLastName(employee.getLastName());
+        return employeeDto;
+    }
+}
