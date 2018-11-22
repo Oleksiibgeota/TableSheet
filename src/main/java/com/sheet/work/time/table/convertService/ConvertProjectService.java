a8284ddfdfc0380646184fa92fac4c127b909b15
Oleksii <bgeota@gmail.com>
2018-11-22 18:23:38 +0200

    create class ConverProjectService

---
+++ src/main/java/com/sheet/work/time/table/convertService/ConvertProjectService.java
@@ -0,0 +1,16 @@
+package com.sheet.work.time.table.convertService;
+
+import com.sheet.work.time.table.dto.ProjectDto;
+import com.sheet.work.time.table.vo.Project;
+
+public class ConvertProjectService implements ConvertServiceProject{
+    @Override
+    public Project convertProject(Project project) {
+        return null;
+    }
+
+    @Override
+    public ProjectDto convertProject(ProjectDto projectDto) {
+        return null;
+    }
+}
